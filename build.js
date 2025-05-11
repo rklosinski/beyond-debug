0e25827fb61fc4b84bac529628b255c990046c64
Richard Klosinski <rich@rckco.com>
2025-05-11 16:51:31 -0700

    upgrade a few of the packages, removed anything windows dependant

--- build.js
+++ build.js
@@ -1,13 +1,14 @@
-// const  { nativeNodeModulesPlugin }  = require("esbuild-native-node-modules-plugin");
-// const { platform } = require("os");
-// require('esbuild').build({
-//     sourcemap:true,
-//     entryPoints: ['./src/extension.ts'],
-//     bundle: true,
-//     outfile: 'out/extension.js',
-//     plugins: platform=="win32"?[]:[nativeNodeModulesPlugin],
-//     external: ['vscode'],
-//     format:'cjs',
-//     platform:'node'
-//     //loader: { '.png': 'binary' },
-//   }).catch(() => process.exit(1))
+//const  { nativeNodeModulesPlugin }  = require("esbuild-native-node-modules-plugin");
+const { platform } = require("os");
+require('esbuild').build({
+    sourcemap:true,
+    entryPoints: ['./src/extension.ts'],
+    bundle: true,
+    outfile: 'out/extension.js',
+    //plugins: platform=="win32"?[]:[nativeNodeModulesPlugin],
+    external: ['vscode'],
+    format:'cjs',
+    platform:'node',
+  //loader: { '.png': 'binary' },
+  loader: { '.node': 'file' }
+  }).catch(() => process.exit(1))
